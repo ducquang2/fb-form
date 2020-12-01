b92d8de01ec0a77553de548eb81da3bc2a79cab0
ducquang2 <quangbilun@gmail.com>
2020-12-01 20:51:34 +0700

    wut

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
 import React, { Component } from "react";;
+import Login from "./components/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: "",
-        pass: ""
-      };
-  
-      this.handleNameChange = this.handleNameChange.bind(this);
-      this.handlePassChange = this.handlePassChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleNameChange(event) {
-      this.setState({name: event.target.name});
-    }
-  
-    handlePassChange(event) {
-      this.setState({pass: event.target.pass});
-    }
-
-    handleSubmit(event) {
-        alert('An account was submitted: Name is ' + this.state.name + ' pass is ' + this.state.pass);
-        event.preventDefault();
-    }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="email"
-              value={this.state.name}
-              onChange={this.handleNameChange} />
-          </label>
-          <br />
-          <label>
-            Pass:
-            <input
-              type="text"
-              value={this.state.pass}
-              onChange={this.handlePassChange} />
-          </label>
-          < br />
-          <input type="submit" value="Submit" />
-        </form>
+        <Login />
       );
     }
   }
---
+++ src/components/Login.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            password: "",
+            username: ""
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleInputChange(event) {
+        console.log(event);
+        const target = event.target;
+        const value = target.type === 'email' ? target.username : target.password;
+        const name = target.name;
+
+        this.setState({[name]: value});
+    }
+    
+    // handlePassChange(event) {
+    //     console.log(event);
+    //     this.setState({ accessKey: event.target.accessKey });
+    // }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.username + ' Pass: ' + this.state.password);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group >
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control 
+                    name="username"
+                    type="email"
+                    value={this.state.username}
+                    onChange={this.handleInputChange}  placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group >
+                    <Form.Label>Pass:</Form.Label>
+                    <Form.Control 
+                    name="password"
+                    type="text"
+                    value={this.state.password}
+                    onChange={this.handleInputChange} placeholder="Enter password" />
+                </Form.Group>
+                <Button variant="primary" type="submit" value="Submit">Submit</Button>
+            </Form>
+        );
+    }
+}
