02e92966626c08b9ea078d0da6350ea05173aa6e
Khanh <tran_khanh@outlook.com>
2020-12-07 19:58:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React, { Component } from "react";;
+import React, { Component } from "react";
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
-  
-    render() {
-      return (
-        <SignUp />
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      signUp: false,
+      login: true,
+    };
   }
+
+  updateComponentAppear = (componentName) => {
+    console.log(componentName);
+    if (componentName == "signUp") {
+      his.setState({ signUp: false, login: true });
+    }
+
+    if (componentName == "login") {
+      this.setState({ signUp: true, login: false });
+    }
+  };
+
+  render() {
+    const { signUp, login } = this.state;
+    return (
+      <div>
+        {signUp ? (
+          <SignUp updateComponentAppear={this.updateComponentAppear} />
+        ) : (
+          ""
+        )}
+        {login ? (
+          <Login updateComponentAppear={this.updateComponentAppear} />
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+}
 export default App;
